111f4833c72cd198450785a2e6fbb4171584d5f3
Artem Rybin <catobazzz@gmail.com>
2025-01-12 20:31:59 +0300

    header: add Header.tsx

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,19 @@
+
+
+export const Header = () => {
+    return (
+        <header>
+            <a href="">
+                <img src="" alt=""/>
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Skills</a></li>
+                    <li><a href="">Testimony</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
