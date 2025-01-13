df4ae07159c301c4a723835953274d98d4ac74d6
Artem Rybin <catobazzz@gmail.com>
2025-01-13 13:36:18 +0300

    main: Main.tsx upd, add FlexWrapper.tsx

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,21 +1,37 @@
 import styled from 'styled-components';
 import photo from './../../../assets/image/photo-1.webp';
+import { FlexWrapper } from '../../../components/FlexWrapper.tsx';
 
 export const Main = () => {
   return (
-    <div>
-      <div>
-        <span>Hi There</span>
-        <h2>I am Svetlana Dyablo</h2>
-        <h1>A Web Developer. </h1>
-      </div>
-      <Photo src={photo} alt="" />
-    </div>
+    <StyledMain>
+      <FlexWrapper alignItems={'center'} justifyContent={'space-around'}>
+        <div>
+          <span>Hi There</span>
+          <Name>I am Svetlana Dyablo</Name>
+          <MainTitle>A Web Developer. </MainTitle>
+        </div>
+        <Photo src={photo} alt="" />
+      </FlexWrapper>
+    </StyledMain>
   );
 };
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #d1b878;
+`;
 
 const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+`;
+
+const MainTitle = styled.h1`
+ //
+`;
+
+const Name = styled.h2`
+ //
 `;
