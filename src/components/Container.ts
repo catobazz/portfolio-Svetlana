441948db4dd70b62b08c236d4ccbfe618ccd9309
Artem Rybin <catobazzz@gmail.com>
2025-02-04 15:59:33 +0300

    feat: Container.ts ref: Header.tsx

---
+++ src/components/Container.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+max-width: 1170px;
+    width: 100%;
+    margin: 0 auto;
+    min-height: 15px;
+    padding: 0 15px;
+    border: 1px solid red;
+`
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,20 +1,31 @@
 import styled from 'styled-components';
-import { Logo } from '../../components/logo/Logo.tsx';
-import { Menu } from '../../components/menu/Menu.tsx';
+import {Logo} from '../../components/logo/Logo.tsx';
+import {Container} from '../../components/Container.ts';
+import {FlexWrapper} from '../../components/FlexWrapper.tsx';
+import {HeaderMenu} from './headerMenu/HeaderMenu.tsx';
 
 const items = ['home', 'skills', 'works', 'contact'];
 
 export const Header = () => {
-  return (
-    <StyledHeader>
-      <Logo />
-      <Menu menuItems={items}/>
-    </StyledHeader>
-  );
+    return (
+        <StyledHeader>
+            <Container>
+                <FlexWrapper justifyContent={'space-between'} alignItems={'center'}>
+                    <Logo/>
+                    <HeaderMenu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
+        </StyledHeader>
+    );
 };
 
 const StyledHeader = styled.header`
-  background-color: #88f67d;
-    display: flex;
-    justify-content: space-between;
+    background-color: rgba(31, 31, 32, 0.9);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 9999;
+    padding: 20px 0;
+
 `;
