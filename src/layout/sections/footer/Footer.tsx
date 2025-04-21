ee8d09f0ecf61a1372c4f9872b2ec2a3891ec462
Artem Rybin <124868063+catobazz@users.noreply.github.com>
2025-04-21 21:25:59 +0300

    ref: Footer.tsx

--- src/layout/sections/footer/Footer.tsx
+++ src/layout/sections/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { Icon } from '../../../components/icon/Icon.tsx'
-import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import { FlexWrapper } from '../../../components/FlexWrapper.tsx'
+import { theme } from '../../../styles/Theme.ts'
 
 export const Footer = () => {
   return (
@@ -9,47 +10,77 @@ export const Footer = () => {
         <Name>Svetlana</Name>
         <SocialList>
           <SocialItem>
-            <SociaLink>
+            <SocialLink>
               <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconId={'istaSvg'} />
-            </SociaLink>
+            </SocialLink>
           </SocialItem>
           <SocialItem>
-            <SociaLink>
-              <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconId={'telegramSvg'} />
-            </SociaLink>
+            <SocialLink>
+              <Icon
+                height={'21px'}
+                width={'21px'}
+                viewBox={'0 0 21px 21px'}
+                iconId={'telegramSvg'}
+              />
+            </SocialLink>
           </SocialItem>
           <SocialItem>
-            <SociaLink>
+            <SocialLink>
               <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconId={'vkSvg'} />
-            </SociaLink>
+            </SocialLink>
           </SocialItem>
           <SocialItem>
-            <SociaLink>
+            <SocialLink>
               <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconId={'linkedSvg'} />
-            </SociaLink>
+            </SocialLink>
           </SocialItem>
         </SocialList>
         <Copyright>© 2022 Svetlana Dyablo</Copyright>
       </FlexWrapper>
-
     </StyledFooter>
   )
 }
 
 const StyledFooter = styled.div`
-  min-height: 20vh;
-  background-color: #834a36;
+  background-color: ${theme.colors.primary};
+  padding: 40px 0;
 `
 
-const Name = styled.span``
+const Name = styled.span`
+  font-family: josefin-sans, sans-serif;
+  font-weight: 700;
+  font-size: 22px;
+  letter-spacing: 3px;
+`
 
 const SocialList = styled.ul`
-display: flex;
-gap: 30px;
+  display: flex;
+  gap: 20px;
+  margin: 30px 0;
 `
 
 const SocialItem = styled.li``
 
-const SociaLink = styled.a``
+const SocialLink = styled.a`
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
 
-const Copyright = styled.small``
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${theme.colors.accent};
+  
+  &:hover {
+    color: ${theme.colors.primary};
+    transform: translateY(-4px);
+  }
+`
+
+const Copyright = styled.small`
+  font-weight: 400;
+  font-size: 12px;
+  text-align: center;
+  opacity: 0.5;
+`
